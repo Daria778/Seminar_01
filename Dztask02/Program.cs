0ada922dbe4b05f417aef21dc9fa78e38f834c06
Daria <shkatov.pad@gmail.com>
2023-02-13 23:45:04 +0300

    Dztask02

---
+++ Dztask02/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+Console.WriteLine("Enter the numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    int max = numberA;
+    Console.WriteLine($"{max} > {numberB}");
+}
+else
+{
+    int max = numberB;
+    Console.WriteLine($"{max} > {numberA}");
+}
