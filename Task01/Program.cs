f1b70190ace987bc75dc26bc4da2758c97e00a9c
Daria <shkatov.pad@gmail.com>
2023-02-13 22:28:44 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// 
+Console.WriteLine("Enter the numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+{
+if(numberB == numberA * numberA)
+{
+    Console.WriteLine($"{numberB} is a square of {numberA}");
+}
+else
+{
+    Console.WriteLine($"{numberB} is not a square of {numberA}");
+}
+}
