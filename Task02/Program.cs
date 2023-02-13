36fef9c724d60f85d2c21552fb07db66ebf22016
Daria <shkatov.pad@gmail.com>
2023-02-13 23:03:47 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿// 
+
+Console.WriteLine("Enter the number of a day of a week(1-7)");
+int number_of_a_week = Convert.ToInt32(Console.ReadLine());
+if(number_of_a_week == 1) Console.WriteLine("Monday");
+else if(number_of_a_week == 2) Console.WriteLine("Tuesday");
+else if(number_of_a_week == 3) Console.WriteLine("Wednesday");
+else if(number_of_a_week == 4) Console.WriteLine("Thursday");
+else if(number_of_a_week == 5) Console.WriteLine("Friday");
+else if(number_of_a_week == 6) Console.WriteLine("Saturday");
+else if(number_of_a_week == 7) Console.WriteLine("Sunday");
