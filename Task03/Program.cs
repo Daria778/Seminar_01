2d265a4c708cc097ef227d0361b60ac9663fd945
Daria <shkatov.pad@gmail.com>
2023-02-13 23:17:15 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,10 @@
+﻿// 
+
+Console.WriteLine("Enter N number");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while(count <= num)
+{
+   Console.Write(count+" ");
+   count++;
+}
