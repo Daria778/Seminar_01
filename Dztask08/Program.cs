4c89bea15a8a2d49df7191bbccb6e9de24ccac1b
Daria <shkatov.pad@gmail.com>
2023-02-14 17:28:00 +0300

    Dztask08

---
+++ Dztask08/Program.cs
@@ -0,0 +1,9 @@
+﻿// 0, 2, ... N
+Console.WriteLine("Enter a number");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while(count <= num)
+{
+   Console.Write(count+ " ");
+   count = count + 2;
+}
