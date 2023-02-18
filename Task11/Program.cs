1a0331c45dd61186ba0ebae30315af3a45422656
Daria <shkatov.pad@gmail.com>
2023-02-19 01:10:48 +0300

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 456 => 46 
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"random number from the segment 100 - 999 => {number}");
+
+//int firstDigit = number / 100;
+//int secondDigit = number % 10;
+//int result = firstDigit * 10 + secondDigit;
+//Console.WriteLine(result);
+
+int newNumber = NewNumber(number);
+Console.WriteLine($"new number from the first received is {newNumber}");
+
+int NewNumber(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
