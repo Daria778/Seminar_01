ce95608337b275e2ea8b2ac981a6a3b06e10dfe5
Daria <shkatov.pad@gmail.com>
2023-02-19 16:29:59 +0300

    Task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,16 +1,29 @@
 ﻿// multiplicity
+Console.WriteLine();
+Console.WriteLine("Remember, that the first number must be more than the second number");
+Console.WriteLine();
 
 Console.WriteLine("Enter the first number");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = Multiplicity(firstNumber,secondNumber);
-if(result == 0) Console.WriteLine($"{firstNumber} is multiple of {secondNumber}");
+if(firstNumber >= secondNumber)
+{
+
+int result = Multiplicity(firstNumber, secondNumber);
+if (result == 0) Console.WriteLine($"{firstNumber} is multiple of {secondNumber}");
 else Console.WriteLine($"{firstNumber} is not multiple of {secondNumber}, the remainder is {result}");
 
-int Multiplicity(int num1,int num2)
+
+int Multiplicity(int num1, int num2)
 {
-    int result = num1 % num2;   
-    return result;
+    return num1 % num2;
+
 }
+}
+else
+{
+    Console.WriteLine("Are you stupid?");
+}
+
