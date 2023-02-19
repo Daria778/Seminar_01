30dec4d30d39e5fa8d5deefd0871e5c388ba29cd
Daria <shkatov.pad@gmail.com>
2023-02-19 22:48:09 +0300

    Task13

---
+++ Dztask13/Program.cs
@@ -0,0 +1,20 @@
+﻿// 356 => 6
+
+int number = new Random().Next();
+Console.WriteLine($"random number => {number}");
+
+while(number > 999)
+{
+    number = number / 10;
+}
+Console.WriteLine(number);
+
+int thirdDigit = ThirdDigit(number);
+Console.WriteLine($"Max digit of the number is {thirdDigit}");
+
+int ThirdDigit(int num)
+{
+    int digit = num % 10;
+    return digit;
+
+}
