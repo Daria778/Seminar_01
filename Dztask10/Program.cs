f2b4b6b01b35dfd7e02f9ecf3fc44cf9a565b714
Daria <shkatov.pad@gmail.com>
2023-02-19 22:33:41 +0300

    Tasks

---
+++ Dztask10/Program.cs
@@ -0,0 +1,13 @@
+﻿// 256 => 5
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"random number from the segment 100 - 999 => {number}");
+
+int newNumber = NewNumber(number);
+Console.WriteLine($"new number from the first received is {newNumber}");
+
+int NewNumber(int num)
+{
+    
+}
+
---
+++ Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// square
+
+Console.WriteLine("Enter the first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(Square(firstNumber, secondNumber)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+bool Square(int num1, int num2)
+{
+    return (num1 == num2 * num2) || (num2 = num1 * num1);
+}
+
