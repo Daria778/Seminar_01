9656d4ac2ad6ccd4eb64a3dbf33987ff147d516b
Daria <shkatov.pad@gmail.com>
2023-02-19 17:55:14 +0300

    Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿// multiplicity2.0
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity2numbers(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+if(Multiplicity2numbers(number)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
