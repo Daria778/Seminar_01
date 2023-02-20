34dc2232f10dce7ed05b681ef3db397b83cbcf28
Daria <shkatov.pad@gmail.com>
2023-02-20 19:45:57 +0300

    Dztask15

---
+++ Dztask15/Program.cs
@@ -0,0 +1,17 @@
+﻿// week
+
+Console.WriteLine("Enter the number of a day of a week");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 1 && number <= 7)
+{
+    int result = Weeknum(number);
+    if(Weeknum(number) >= 6) Console.WriteLine("Weekend");
+
+
+
+}
+else
+{
+    Console.WriteLine("You have entered the wrong number");
+}
