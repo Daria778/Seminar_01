83cafbf81840762370a631759f7c48ce3f34ebef
Daria <shkatov.pad@gmail.com>
2023-02-22 19:42:30 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1, 4, 9, .......
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Square(number);
+
+void Square(int num)
+{
+   int index = 1;
+    while(index <= num)
+    {
+       
+       Console.WriteLine($"{index} - {index * index}");
+       index++;
+    }
+    return;
+}
