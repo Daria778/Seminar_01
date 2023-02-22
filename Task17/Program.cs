40e4fb1f144d25b961364313b2b5c061b065ef6d
Daria <shkatov.pad@gmail.com>
2023-02-22 16:29:23 +0300

    Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,22 @@
+﻿// (x;y)
+
+Console.WriteLine("Enter coordinates");
+Console.Write("x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 
+                 ? $"Entered coordinates corespond to {quarter} quarter"
+                 : "Entered wrong coordinates";
+Console.WriteLine(result);
+
+int Quarter(int x1, int y1)
+{
+if(x1 > 0 && y1 > 0) return 1;
+if(x1 < 0 && y1 > 0) return 2;
+if(x1 < 0 && y1 < 0) return 3;
+if(x1 > 0 && y1 < 0) return 4;
+return 0;
+}
