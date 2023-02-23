826f210f3e83145278c8555ff7688f2e1723a487
Daria <shkatov.pad@gmail.com>
2023-02-23 03:59:04 +0300

    Dztask23

---
+++ Dztask23/Program.cs
@@ -0,0 +1,18 @@
+﻿//1, 8, 27, .......
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Square(number);
+
+void Square(int num)
+{
+   int index = 1;
+    while(index <= num)
+    {
+       
+       Console.WriteLine($"{index} - {index * index * index}");
+       index++;
+    }
+    return;
+}
