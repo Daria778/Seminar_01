8a86dbcd814f1651e0f859eb50b359d7da82da18
Daria <shkatov.pad@gmail.com>
2023-02-25 20:29:43 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// 956 = 3, 1275 = 4....
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digits = Digits(number);
+Console.WriteLine(digits);
+
+int Digits(int num)
+{
+    int result = 0;
+    while(num != 0)
+    {
+        num = num / 10;
+        result++;
+    }
+    return result;
+}
