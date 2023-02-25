e8b3c6c2fdf7a0765ae759da008ecf309c15b78a
Daria <shkatov.pad@gmail.com>
2023-02-25 22:33:24 +0300

    Dztask29

---
+++ Dztasks/Dztask29/Program.cs
@@ -0,0 +1,23 @@
+﻿// 
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next();
+    }
+}
+
+void PrintArray(int[] ar)
+{
+    for (int pos = 0; pos < ar.Length; pos++)
+    {
+        Console.Write(ar[pos]+" ");
+       
+    }
+}
