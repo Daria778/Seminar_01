83bfcceffe9661cfe0980dd5c2ce5413737eee35
Daria <shkatov.pad@gmail.com>
2023-02-25 20:51:36 +0300

    Task28

---
+++ Tasks/Task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// N!
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Factorial(number);
+Console.WriteLine(result);
+
+int Factorial(int num)
+{
+    int n = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            n = n * i;
+        }
+    }
+    return n;
+}
