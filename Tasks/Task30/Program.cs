79cc72fbc197ad51bdab9cbe30ca5f845e15d887
Daria <shkatov.pad@gmail.com>
2023-02-25 22:23:02 +0300

    Task30

---
+++ Tasks/Task30/Program.cs
@@ -0,0 +1,26 @@
+﻿// 
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] ar)
+{
+    for (int pos = 0; pos < ar.Length; pos++)
+    {
+        Console.Write(ar[pos]+" ");
+       
+    }
+}
+
+
+
