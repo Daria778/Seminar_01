c4cb59e5b0a06497d00b0b800cca813587b95b70
Daria <shkatov.pad@gmail.com>
2023-02-25 19:57:02 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+Console.WriteLine("Enter a number");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumbers(numberA);
+Console.WriteLine(result);
+
+int SumNumbers(int numA)
+{
+    int sum = 0;
+    for (int i = 1; i <= numA; i++)
+    {
+        checked
+        {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
