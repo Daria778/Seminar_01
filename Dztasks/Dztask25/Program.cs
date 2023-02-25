f9fb7d1bb353b66ba40a27286f7bc541189e912c
Daria <shkatov.pad@gmail.com>
2023-02-26 02:05:01 +0300

    Dztask25

---
+++ Dztasks/Dztask25/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+Console.WriteLine("Enter number A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int method = Method(numberA, numberB);
+Console.WriteLine(method);
+
+int Method(int numA, int numB)
+{   int res = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        res = res * numA;
+    }
+    return res;
+}
