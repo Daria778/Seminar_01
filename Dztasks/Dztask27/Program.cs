8b193060e6d74dceab264638d204ae221862efa8
Daria <shkatov.pad@gmail.com>
2023-02-26 03:35:00 +0300

    Dztask27

---
+++ Dztasks/Dztask27/Program.cs
@@ -0,0 +1,19 @@
+﻿// 452 = 11, sum
+
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = Sum(number);
+Console.WriteLine(sum);
+
+int Sum(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        int d = num % 10;
+        result = result + d;
+        num = num / 10;
+    }
+    return result;
+}
