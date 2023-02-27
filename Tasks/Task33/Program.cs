7d614e98ba4865cc934ec85e8eeea62dd2c69956
Daria <shkatov.pad@gmail.com>
2023-02-27 20:54:50 +0300

    Task33

---
+++ Tasks/Task33/Program.cs
@@ -0,0 +1,45 @@
+﻿// 
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(12, -9, 9);
+PrintArray(array);
+Console.WriteLine();
+if (FindI(array, number)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+PrintArray(array);
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ", ");
+        else
+            Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+bool FindI(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == num) return true;
+    }
+    return false;
+}
