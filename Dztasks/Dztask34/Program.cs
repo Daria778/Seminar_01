350aab3578d8847e7b6f02513f980f4498484626
Daria <shkatov.pad@gmail.com>
2023-03-02 23:35:34 +0300

    Dztask34

---
+++ Dztasks/Dztask34/Program.cs
@@ -0,0 +1,41 @@
+﻿// [345, 897, 568, 234] -> 2
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int[] array = FillArray(4, 100, 999);
+PrintArray(array);
+
+Console.Write($" -> {EvenNumbered(array)}");
+
+int EvenNumbered(int[]arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ", ");
+        else
+            Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
