0c6f797c2d2b7592020dcc738efe958271b8cfa4
Daria <shkatov.pad@gmail.com>
2023-03-05 15:54:16 +0300

    Tasks

---
+++ Tasks/Task39/Program.cs
@@ -0,0 +1,44 @@
+﻿//
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int[] array = FillArray(5, 0, 10);
+Console.WriteLine("Your array:");
+PrintArray(array);
+Console.WriteLine();
+//Array.Reverse(array);
+ReverseArray(array);
+PrintArray(array);
+
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ", ");
+        else
+            Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
---
+++ Tasks/Task40/Program.cs
@@ -0,0 +1,18 @@
+﻿// 
+
+Console.WriteLine("Enter the number A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number C");
+int numberC = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ABC(numberA, numberB, numberC));
+bool ABC(int numA, int numB, int numC)
+{
+    if(numA < numB + numC && numB < numA + numC && numC < numA + numB) return true;
+    else return false;
+   
+
+}
