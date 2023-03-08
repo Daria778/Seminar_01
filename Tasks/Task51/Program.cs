e2691115ede207aa90c1140e9fdaa6ba6b4707f5
Daria <shkatov.pad@gmail.com>
2023-03-08 15:49:11 +0300

    Task51

--- Tasks/Task51/Program.cs
+++ Tasks/Task51/Program.cs
@@ -4,45 +4,41 @@ void PrintArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        Console.Write("[");
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            if(i < arr.GetLength(1) - 1)
-        Console.Write(arr[i, j]+", ");
-       else
-        Console.Write(arr[i, j]);
+                Console.Write($"{arr[i, j],4} ");
         }
-         Console.WriteLine("]");
+        Console.WriteLine();
     }
 }
 
 int[,] FillArray(int rows, int columns, int min, int max)
 {
-   int[,] arr = new int[rows, columns];
-   Random rnd = new Random();
-   for (int i = 0; i < arr.GetLength(0); i++)
-   {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    int[,] arr = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-         arr[i, j] = rnd.Next(min, max + 1);
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
     }
-   } 
-   return arr;
+    return arr;
 }
 
 int Sum(int[,] arr)
 {
     int sum = 0;
     int size = arr.GetLength(0);
-    if(size > arr.GetLength(1)) size = arr.GetLength(1);
+    if (size > arr.GetLength(1)) size = arr.GetLength(1);
     for (int i = 0; i < size; i++)
-   {
-          sum += arr[i, j];
+    {
+        sum += arr[i, i];
     }
-   return sum;
+    return sum;
 }
 
-int [,] array = FillArray(3, 4, 1, 10);
+int[,] array = FillArray(4, 3, 1, 10);
 PrintArray(array);
 Console.WriteLine();
 Console.WriteLine(Sum(array));
