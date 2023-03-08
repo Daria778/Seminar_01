f2bd1e10a98fa4b5791e17baa44a3b101ce58f65
Daria <shkatov.pad@gmail.com>
2023-03-08 17:02:14 +0300

    Dztask47

---
+++ Dztasks/Dztask47/Program.cs
@@ -0,0 +1,31 @@
+﻿// 
+
+
+double[,] FillArray(int rows, int columns, int min, int max)
+{
+    double[,] arr = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+                Console.Write($"{Math.Round(arr[i, j], 1, MidpointRounding.ToZero), 3}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = FillArray(3, 4, 1, 10);
+PrintArray(array);
