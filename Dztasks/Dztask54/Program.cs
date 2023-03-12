2feee617b9b87d33df4a4e12c8d870ffcbab94bc
Daria <shkatov.pad@gmail.com>
2023-03-13 02:08:37 +0300

    j

---
+++ Dztasks/Dztask54/Program.cs
@@ -0,0 +1,54 @@
+﻿// 
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] arr = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4} ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+void Sort(int[,] arr)
+{
+    int max = arr[0, 0];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int n = j + 1; n < arr.GetLength(1); n++)
+            {
+                if (max < arr[i, j])
+                    max = arr[i, j];
+            }
+            int temp = max;
+            max = arr[i, j];
+            arr[i, j] = temp;
+        }
+    }
+}
+
+
+int[,] array = FillArray(3, 4, 1, 10);
+PrintArray(array);
+Console.WriteLine();
+Sort(array);
+PrintArray(array);
---
+++ Dztasks/Dztask56/Program.cs
@@ -0,0 +1,54 @@
+﻿// 
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] arr = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4} ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
+
+int Sum(int[,] arr)
+{
+    int minSum = 0;
+    int sum = 0;
+    int count = 1;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+        }
+        if (minSum > sum)
+        {
+            minSum = sum;
+            count++;
+        }
+        sum = 0;
+    }
+    return count;
+}
+
+int[,] array = FillArray(3, 4, 1, 10);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(Sum(array));
