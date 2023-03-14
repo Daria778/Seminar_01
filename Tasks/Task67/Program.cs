0e543091bac24667275d9b310a7f0380d9715b09
Daria <shkatov.pad@gmail.com>
2023-03-14 19:17:55 +0300

    Task67

---
+++ Tasks/Task67/Program.cs
@@ -0,0 +1,13 @@
+﻿// 453 -> 12, sum
+Console.WriteLine("Enter a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int SumNumbers(int n)
+{
+if (n == 0) return n;
+else return n % 10 + SumNumbers(n / 10); // 453 / 10, 45 / 10, 4 / 10(save in stack) call is first always!!!
+                                         // 4 + 5 + 3 
+}
+
+Console.WriteLine(SumNumbers(453));
