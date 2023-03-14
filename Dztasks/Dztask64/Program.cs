eca928026f53fa059dc5d218f1d1c9ca51310927
Daria <shkatov.pad@gmail.com>
2023-03-14 21:31:10 +0300

    l

---
+++ Dztasks/Dztask64/Program.cs
@@ -0,0 +1,14 @@
+﻿// n - 1
+
+Console.WriteLine("Enter N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+
+}
+if(n > 0) NaturalNumbers(n);
+else Console.WriteLine("You have entared the wrong number");
