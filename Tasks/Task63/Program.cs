6fa579c3e172f842331874b86c0decd5d04cfc9e
Daria <shkatov.pad@gmail.com>
2023-03-14 18:11:41 +0300

    t

---
+++ Tasks/Task63/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1 - N
+
+Console.WriteLine("Enter N ");
+int n = int.Parse(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+    NaturalNumbers(num -1);
+    Console.Write($"{num} ");
+}
+
+NaturalNumbers(n);
